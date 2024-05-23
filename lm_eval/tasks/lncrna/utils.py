d2fddafebbf73af3ca8578d753e0a637fe71f5a9
Nick Doiron <ndoiron@mapmeld.com>
2024-05-23 18:09:01 -0500

    fix

--- lm_eval/tasks/lncrna/utils.py
+++ lm_eval/tasks/lncrna/utils.py
@@ -1,6 +1,10 @@
 qed = "∎"  # from math symbols
 
 
+def doc_to_target(doc) -> str:
+    return ["No", "Yes"][doc["label"]]
+
+
 def doc_to_text_context(doc) -> str:
     sequence = doc["sequence"]
     return f"""Is the following sequence long non-coding RNA (LncRNA)?
